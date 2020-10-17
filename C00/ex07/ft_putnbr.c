1722ef19bbcb2773a451f7d3c061f82a944cf649
Jaewook Kim <jaewkim@c8r3s12.42seoul.kr>
2020-10-18 06:10:11 +0900

    ex08

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -1,35 +1,63 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaewkim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/18 03:48:05 by jaewkim           #+#    #+#             */
+/*   Updated: 2020/10/18 04:04:51 by jaewkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-void ft_putnbr(int nb);
+void	ft_putnbr(int nb);
+void	store_arr_print(int o_num, int minus, int add);
 
-int main ()
+int		main(void)
 {
-	ft_putnbr(123);
-	
-	return 0;
+	ft_putnbr(2147483647);
+	return (0);
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-	int *a;
-	int i;
-	int b;
+	int add;
+	int index;
+	int minus;
 
-	b=1;
-	i = 0;
-	
-	
-	a = &nb;
-	
-	while(a[i])
+	add = nb == -2147483648 ? 1 : 0;
+	index = 0;
+	if (nb < 0)
 	{
-		write(1, &a[i++]+48, 1);
+		nb = add == 1 ? nb + 1 : nb;
+		nb = nb * -1;
+		minus = 1;
 	}
+	store_print(nb, minus, add);
+}
 
-	write(1, "\n", 1);
-	write(1, "c", 1);	
-	printf("%c",b);
+void	store_print(int o_num, int minus, int add)
+{
+	int		index;
+	char	num_arr[10];
+
+	index = 0;
+	while (o_num > 0)
+	{
+		num_arr[index] = o_num % 10 + 48;
+		o_num /= 10;
+		index++;
+	}
+	if (minus == 1)
+	{
+		if (add == 1)
+			num_arr[0] += 1;
+		write(1, "-", 1);
+	}
+	while (--index >= 0)
+	{
+		write(1, &num_arr[index], 1);
+	}
 }
--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,13 +1,73 @@
 #include <unistd.h>
 
-void ft_print_combn(int n);
+void	ft_print_combn(int n);
+void	recur(char a_num[9], int covereage);
+int 	max_check(char a_num[9], int covereage);
+void	eachnum_up(char a_num[9], int index_num, int cover_num);
+void	show_arr(char a_num[9], int cover_num);
 
-int main ()
+
+int		main ()
 {
 	ft_print_combn(6);
 }
 
-void ft_print_combn(int n);
+void	ft_print_combn(int n)
 {
-	
+	int i;
+	char a_num[9];
+	i = 0;
+	while (i < n)
+	{
+		if (i == n-1)
+			a_num[i] = '1';
+		else
+			a_num[i] = '0';
+		i++;
+	}
+	recur(a_num, n);
+}
+
+void	recur(char a_num[9], int covereage)
+{
+	if (max_check(a_num, covereage) == 0)
+		return;
+	else
+		return recur(a_num, covereage);
+}
+
+int		max_check(char a_num[9], int covereage)
+{
+	int max_num;
+	int arr_num;
+
+	arr_num = covereage;
+	max_num = 9;
+	while (--covereage >= 0)
+	{
+		if (a_num[covereage] != max_num--)
+		{
+			eachnum_up(a_num, covereage, arr_num);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+void	eachnum_up(char a_num[9], int index_num, int cover_num)
+{
+	a_num[index_num] += 1;
+	while (index_num++ != cover_num)
+	{
+		a_num[index_num] += 1;
+	}
+}
+
+void	show_arr(char a_num[9], int cover_num) 
+{
+	while(cover_num-- >= 0)
+	{
+		write(1, &a_num[cover_num], 1);
+	}
+	write(1, ", ", 2);
 }

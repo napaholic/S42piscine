9177c4150ae949d0e7c13321739d85edea80e97e
Jiyeon Park <jiyepark@c8r3s14.42seoul.kr>
2020-10-17 23:43:55 +0900

    rushfinal

--- Rush00/rush01.c
+++ Rush00/rush01.c
@@ -1,39 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush00.c                                           :+:      :+:    :+:   */
+/*   rush01.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jiyepark <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/17 17:09:12 by jiyepark          #+#    #+#             */
-/*   Updated: 2020/10/17 23:38:09 by jiyepark         ###   ########.fr       */
+/*   Created: 2020/10/17 14:33:18 by jiyepark          #+#    #+#             */
+/*   Updated: 2020/10/17 23:37:08 by jiyepark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char a);
+void	get_location01(int a, int b, int i, int j);
 
-void	rush00(int x, int y)
+void	rush01(int a, int b)
 {
 	int i;
 	int j;
 
 	i = 0;
-	while (i < y)
+	while (i < b)
 	{
 		j = 0;
-		while (j < x)
+		while (j < a)
 		{
-			if ((i == 0 || i == y - 1) && (j == 0 || j == x - 1))
-				ft_putchar('o');
-			else if (i == 0 || i == y - 1)
-				ft_putchar('-');
-			else if (j == 0 || j == x - 1)
-				ft_putchar('|');
-			else
-				ft_putchar(' ');
+			get_location01(a, b, i, j);
 			j++;
 		}
 		ft_putchar('\n');
 		i++;
+	}
+}
+
+void	get_location01(int a, int b, int i, int j)
+{
+	if ((i == 0 && j == 0) || (i == b - 1 && j == a - 1 && i != 0 && j != 0))
+	{
+		ft_putchar('/');
+	}
+	else if ((i == 0 && j == a - 1) || (i == b - 1 && j == 0))
+	{
+		ft_putchar('\\');
+	}
+	else if ((i == 0 || i == b - 1) || (j == 0 || j == a - 1))
+	{
+		ft_putchar('*');
+	}
+	else
+	{
+		ft_putchar(' ');
 	}
 }

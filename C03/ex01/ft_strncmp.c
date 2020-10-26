b2f5643b0685110964b1304a4c13935144c9eac1
Jaewook Kim <jaewkim@c10r7s14.42seoul.kr>
2020-10-26 23:02:31 +0900

    c03

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,13 +6,13 @@
 /*   By: mjay <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/24 04:26:57 by mjay              #+#    #+#             */
-/*   Updated: 2020/10/26 14:39:26 by jaewkim          ###   ########.fr       */
+/*   Updated: 2020/10/26 23:01:27 by jaewkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	i = -1;
 	while (*s1 && *s2 && (++i < n))

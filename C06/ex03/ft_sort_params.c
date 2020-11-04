60a9223ed44dd279c46d0c95f99c6a8739757a04
Jaewook Kim <jaewkim@c10r6s14.42seoul.kr>
2020-11-04 16:34:41 +0900

    c06

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -6,33 +6,56 @@
 /*   By: jaewkim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 14:22:14 by jaewkim           #+#    #+#             */
-/*   Updated: 2020/11/03 00:36:22 by jaewkim          ###   ########.fr       */
+/*   Updated: 2020/11/04 16:33:10 by jaewkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include <unistd.h>
 
-void	sort_content(char *con)
+int		sort_content(char *con, char *con1)
 {
 	int		i;
-	int		j;
-	char	c;
-
+	
 	i = 0;
-	while (con[i])
+	while (con[i] || con1[i])
 	{
-		j = 1;
-		while (con[j])
-		{
-			if (con[j - 1] > con[j])
-			{
-				c = con[j];
-				con[j] = con[j - 1];
-				con[j - 1] = c;
-			}
-			j++;
-		}
+		if (con[i] > con1[i])
+			return (1);
+		if (con[i] < con1[i])
+			return (-1);
 		i++;
+	}
+	return (0);
+}
+
+void	sort_array(int ac, char **av)
+{
+	int i;
+	int j;
+	char *tmp;
+	int idx;
+
+	tmp = 0;
+	i = 0;
+	idx = 0;
+	while (av[i])
+		i++;
+	i--;
+	j = i;
+	while (idx < ac)
+	{
+		i = j;
+		while (*av[i] && i <= 1)
+		{
+			if (sort_content(av[i - 1], (av[i])) > 0)
+			{
+				tmp = av[i];
+				av[i] = av[i - 1];
+				av[i - 1] = tmp;
+			}
+			i--;
+		}
+		idx++;
 	}
 }
 
@@ -42,12 +65,11 @@ int		main(int ac, char **av)
 	int j;
 
 	ac++;
-	i = 0;
+	sort_array(ac, av);	
 	j = 0;
-	while (av[++i])
-		sort_content(av[i]);
-	i = 0;
-	while (av[++i])
+	i = 1;
+	
+	while (av[i])
 	{
 		j = 0;
 		while (av[i][j])
@@ -55,6 +77,7 @@ int		main(int ac, char **av)
 			write(1, &av[i][j], 1);
 			j++;
 		}
+		i++;
 		write(1, "\n", 1);
 	}
 	return (0);
